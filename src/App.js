34843ed32cfccb6241a14f33201d262227155598
unknown <adityamantri05@gmail.com>
2020-06-06 17:34:19 -0700

    React Redux tutorial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Posts from './components/Posts';
+import React, { Component } from 'react'
+import Postform from './components/Postform';
+
+import { Provider } from 'react-redux'
+import store from './store'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <Postform />
+          <Posts />
+        </div>
+      </Provider>
+    )
+  }
+}
+
+
+export default App;
